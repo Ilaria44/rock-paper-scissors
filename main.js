e1d91067bb815eab4ae3bd8b7b73e950fe47d8ea
Ilaria44 <91695386+Ilaria44@users.noreply.github.com>
2023-11-05 17:16:18 +0100

    add pseudocode for getComputerChoice function

---
+++ main.js
@@ -0,0 +1,5 @@
+//create a function getComputerChoice that returns a random value "rock", "paper" or "scissors"
+//create a function getComputerChoice
+//create a variable num and assign to it a random number between 1 and 3
+//if 1 return rock, else if 2 return paper, else if 3 return scissors
+
