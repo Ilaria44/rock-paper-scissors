ff391fe51c9373dbe753e206f2cd0d4cec693adc
Ilaria44 <91695386+Ilaria44@users.noreply.github.com>
2023-11-05 17:27:02 +0100

    create function getComputerChoice

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
-//create a function getComputerChoice that returns a random value "rock", "paper" or "scissors"
+
 //create a function getComputerChoice
 //create a variable num and assign to it a random number between 1 and 3
 //if 1 return rock, else if 2 return paper, else if 3 return scissors
+
+function getComputerChoice() {
+  let num = Math.floor(Math.random() * 3 + 1);
+
+  let choice;
+
+  if (num === 1) {
+    choice = "Rock";
+  } else if (num === 2) {
+    choice = "Paper";
+  } else if (num === 3) {
+    choice = "Scissors";
+  }
+
+}
+
 
