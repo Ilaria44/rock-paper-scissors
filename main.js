0a394c00109169a6fffa843f97aadc02afc621a8
Ilaria44 <91695386+Ilaria44@users.noreply.github.com>
2024-10-27 19:30:16 +0100

    refactor to make one round work

--- main.js
+++ main.js
@@ -30,215 +30,113 @@ function getComputerChoice() {
 
 }
 
+
+
 //play round by taking in both computer and player choices + change buttons ui + return result message
 
+
 function playRound (e) {
- if(e.target.tagName === "BUTTON") {
-  getComputerChoice();
+  playerButtons.addEventListener("click", playRound);
 
-  switch (computerChoice) {
-    case "rock":
-      computerButtons.children[0].classList.add("active");
-      break;
+  if(e.target.tagName === "BUTTON") {
+    getComputerChoice();
 
-    case "paper":
-      computerButtons.children[1].classList.add("active");
-      break;
-
-    case "scissors":
-      computerButtons.children[2].classList.add("active");
-      break;
-  }
-
-  playerChoice = e.target.id;
-  e.target.classList.add("active");
-
-  playerButtons.removeEventListener("click", playRound);
-
-  let playerRoundMessage = "Congrats! What a lucky round!";
-  let computerRoundMessage = "Oh no! You lost this round!";
-  let tieRoundMessage = "It's a tie! Try again.";
-
-  let roundWinner;
-
-  if (playerChoice === computerChoice) {
-    roundWinner = "tie";
-  } else if ((playerChoice === "scissors" && computerChoice === "paper")
-  || (playerChoice === "paper" && computerChoice === "rock") 
-  || (playerChoice === "rock" && computerChoice === "scissors")) {
-    roundWinner = "player";  
-  } else if ((playerChoice === "scissors" && computerChoice === "rock") 
-  || (playerChoice === "paper" && computerChoice === "scissors")
-  || (playerChoice === "rock" && computerChoice === "paper")) {
-    roundWinner = "computer";
-  }
-
-  let roundWinnerPara = document.createElement("p");
-  
-  switch (roundWinner) {
-    case "tie":
-      roundWinnerPara.textContent = tieRoundMessage.toUpperCase();
-      break;
-
-    case "player":
-      roundWinnerPara.textContent = playerRoundMessage.toUpperCase();
-      playerScore++;
-      break;
-
-    case "computer":
-      roundWinnerPara.textContent = computerRoundMessage.toUpperCase();
-      computerScore++;
-  }
-
-  resultDiv.appendChild(roundWinnerPara);
-
-  playerScoreMessage.textContent = "Your Score: " + playerScore; 
-  computerScoreMessage.textContent = "Computer Score: " + computerScore; 
-
-
- }
-}
-
-
-playerButtons.addEventListener("click", playRound);
-
-//create a function getComputerChoice, random number between 1 and 3 equals to computer choice + make button ui change only after player selection
-
-/* 
-function getComputerChoice() {
-  let computerChoice;
-  let computerButtons = document.querySelector(".buttons-container.computer");
-
-  let num = Math.floor(Math.random() * 3 + 1);
-
-  if (num === 1) {
-    computerChoice = "rock";
-  } else if (num === 2) {
-    computerChoice = "paper";
-  } else if (num === 3) {
-    computerChoice = "scissors";
-  }
-
-
-  playerButtons.addEventListener("click", function changeButtons (e) {
-    if(e.target.tagName === "BUTTON") {
-      if (num === 1) {
+    switch (computerChoice) {
+      case "rock":
         computerButtons.children[0].classList.add("active");
-      } else if (num === 2) {
+        break;
+
+      case "paper":
         computerButtons.children[1].classList.add("active");
-      } else if (num === 3) {
+        break;
+
+      case "scissors":
         computerButtons.children[2].classList.add("active");
-      }
+        break;
     }
 
-    playerButtons.removeEventListener("click", changeButtons);
-   
+    playerChoice = e.target.id;
+    e.target.classList.add("active");
+
+    playerButtons.removeEventListener("click", playRound);
+
+    let playerRoundMessage = "Congrats! What a lucky round!";
+    let computerRoundMessage = "Oh no! You lost this round!";
+    let tieRoundMessage = "It's a tie! Try again.";
+
+    let roundWinner;
+
+    if (playerChoice === computerChoice) {
+      roundWinner = "tie";
+    } else if ((playerChoice === "scissors" && computerChoice === "paper")
+    || (playerChoice === "paper" && computerChoice === "rock") 
+    || (playerChoice === "rock" && computerChoice === "scissors")) {
+      roundWinner = "player";  
+    } else if ((playerChoice === "scissors" && computerChoice === "rock") 
+    || (playerChoice === "paper" && computerChoice === "scissors")
+    || (playerChoice === "rock" && computerChoice === "paper")) {
+      roundWinner = "computer";
+    }
+
+    let roundWinnerPara = document.createElement("p");
     
+    switch (roundWinner) {
+      case "tie":
+        roundWinnerPara.textContent = tieRoundMessage.toUpperCase();
+        break;
 
-  })
+      case "player":
+        roundWinnerPara.textContent = playerRoundMessage.toUpperCase();
+        playerScore++;
+        break;
 
-  return computerChoice;
+      case "computer":
+        roundWinnerPara.textContent = computerRoundMessage.toUpperCase();
+        computerScore++;
+    }
 
-}
+    resultDiv.appendChild(roundWinnerPara);
 
+    playerScoreMessage.textContent = "Your Score: " + playerScore; 
+    computerScoreMessage.textContent = "Computer Score: " + computerScore; 
 
-//get player choice based on which button is clicked + create clicked button ui change + ui change for 
+    let nextRoundBtn = document.createElement("button");
+    nextRoundBtn.textContent = "Next Round";
+    resultDiv.parentElement.parentElement.appendChild(nextRoundBtn);
 
-function getPlayerChoice () {
-let playerChoice;
+    nextRoundBtn.addEventListener("click", () => {
+      Array.from(playerButtons.children).forEach(child => {
+        child.classList.remove("active");
+      });
 
-playerButtons.addEventListener("click", function getSelection (e) {
-  playerChoice = e.target.id;
+      Array.from(computerButtons.children).forEach(child => {
+        child.classList.remove("active");
+      });
 
-  e.target.classList.add("active");
-  playerButtons.removeEventListener("click", getSelection);
+      roundWinnerPara.textContent = "";
+    });
 
-  return playerChoice;
-})
+    return playerScore, computerScore;
 
-
-
-
-}
-
-
-//play round by getting both player and computer choice + show message of round winner
-
-let playerRoundMessage = "Congrats! What a lucky round!";
-let computerRoundMessage = "Oh no! You lost this round!";
-let tieRoundMessage = "It's a tie! Try again.";
-
-
-function playRound(playerSelection, computerSelection) {
- 
-  let roundWinner;
-
-  if (playerSelection === computerSelection) {
-    roundWinner = "tie";
-  } else if ((playerSelection === "scissors" && computerSelection === "paper")
-  || (playerSelection === "paper" && computerSelection === "rock") 
-  || (playerSelection === "rock" && computerSelection === "scissors")) {
-    roundWinner = "player";  
-  } else if ((playerSelection === "scissors" && computerSelection === "rock") 
-  || (playerSelection === "paper" && computerSelection === "scissors")
-  || (playerSelection === "rock" && computerSelection === "paper")) {
-    roundWinner = "computer";
   }
-
-  let roundWinnerPara = document.createElement("p");
-  
-  switch (roundWinner) {
-    case "tie":
-      roundWinnerPara.textContent = tieRoundMessage;
-      break;
-
-    case "player":
-      roundWinnerPara.textContent = playerRoundMessage;
-      break;
-
-    case "computer":
-      roundWinnerPara.textContent = computerRoundMessage;
-  }
-
-  resultDiv.appendChild(roundWinnerPara);
-
-
-  return roundWinner;
 }
 
-playRound(getPlayerChoice(), getComputerChoice());
+
+
+
 
 //create game to play rounds until player or computer reach score 5
 
 function game() {
   let gameWinner;
-
-  /* while() {
-
-  } */
   
-  /* for (let step = 1; step < 6; step++) {
-    let round = playRound(prompt("Round " + step +": make your choice! Will it be rock, paper or scissors?"), getComputerChoice());
+ // while(computerScore < 5 && playerScore < 5) {
+  playerButtons.addEventListener("click", playRound);
 
-    console.log(round);
+  //}
 
-    if(round === playerWinsRound) {
-    score++;
-    } else if (round === invalidRound || round === tieRound) {
-    step--;
-    }
-  }
+}
+
+
+game();
   
-  if (score >= 3) {
-    gameWinner = "Hip Hip Hurray! You're the winner of this game!";
-  } else {
-    gameWinner = "I'm sorry, today is not your lucky day! You can try again to challenge me!"
-  }
-
-  console.log(gameWinner); 
-
-}*/
-
-
-
-//game(); */
